68f968077d830601f05a8f38dc29d400d192d7be
George A. Mihaila <george.mihaila@gmail.com>
2018-04-11 13:27:18 -0700

    name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'apiwrap',
-    version = '0.1.0',
+    version = '0.1.1',
     description = 'A simple HTTP POST API wrapper for Python classes',
     author = 'George A. Mihaila',
     author_email = 'george.mihaila@gmail.com',
