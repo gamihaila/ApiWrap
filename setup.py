82729165bc6d1ceabc84959c8cbb02cea5a9a945
George A. Mihaila <george.mihaila@gmail.com>
2018-04-11 13:12:33 -0700

    setup

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="WrapIt",
-    version="0.1.0",
-    packages=find_packages(),
+    name = 'WrapIt',
+    version = '0.1.0',
+    description = 'A simple HTTP POST API wrapper for Python classes',
+    author = 'George A. Mihaila',
+    author_email = 'george.mihaila@gmail.com',
+    url = 'https://github.com/gamihaila/WrapIt',
+    download_url = 'https://github.com/gamihaila/WrapIt/archive/0.1.0.tar.gz',
+    packages=['wrapit'],
     install_requires=[
         "Flask"
-    ],
+    ]
 )
